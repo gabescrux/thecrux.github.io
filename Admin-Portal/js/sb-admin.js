0f98d444edd53219fdbf09fe6d4f0d8b932af8ed
fmso_domain\ZM09886 <emily.lautenbach@navy.mil>
2019-05-07 11:06:53 -0400

    admin
    scroll, menu fixed

--- Admin-Portal/js/sb-admin.js
+++ Admin-Portal/js/sb-admin.js
@@ -27,7 +27,8 @@
       $('.scroll-to-top').fadeOut();
     }
   });
-
+  
+  
   // Smooth scrolling using jQuery easing
   $(document).on('click', 'a.scroll-to-top', function(event) {
     var $anchor = $(this);
@@ -36,5 +37,45 @@
     }, 1000, 'easeInOutExpo');
     event.preventDefault();
   });
+  
+  
+
+//Select all links with hashes
+$('a[href*="#"]')
+// Remove links that don't actually link to anything
+.not('[href="#0"]')
+.click(function(event) {
+  // On-page links
+  if (
+    location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+    && 
+    location.hostname == this.hostname
+  ) {
+    // Figure out element to scroll to
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    // Does a scroll target exist?
+    if (target.length) {
+      // Only prevent default if animation is actually gonna happen
+      event.preventDefault();
+      $('html, body').animate({
+        scrollTop: target.offset().top
+      }, 1000, function() {
+        // Callback after animation
+        // Must change focus!
+        var $target = $(target);
+        $target.focus();
+        if ($target.is(":focus")) { // Checking if the target was focused
+          return false;
+        } else {
+          $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+          $target.focus(); // Set focus again
+        };
+      });
+    }
+  }
+});
+
+
 
 })(jQuery); // End of use strict
